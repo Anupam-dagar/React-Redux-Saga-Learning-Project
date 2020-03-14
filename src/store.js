0190a233faefa4378b02c9eb8dc1bfbdbb9de974
Anupam Dagar <dagaranupam@gmail.com>
2020-03-14 23:00:45 +0800

    minor fix

--- src/store.js
+++ src/store.js
@@ -12,8 +12,7 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(sagaMiddleware, websocketmiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(sagaMiddleware, websocketmiddleware)
   )
 );
 
